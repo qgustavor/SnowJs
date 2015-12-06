e563245d35588532917e1137fa57b481dd22f426
Gustavo Rodrigues <qgustavor@gmail.com>
2015-12-06 15:23:36 -0200

    Update snow.js

--- snow.js
+++ snow.js
@@ -18,9 +18,9 @@
   })();
 
   // constants and storage for objects that represent snow flake positions
-  var DEPTH = 20,
-      UNITS = 50,
-      Z = 0.015,
+  var DEPTH = 10,
+      UNITS = 70,
+      DELTA = 0.015,
       flakes = [];
 
   // function to reset a flake object
@@ -37,6 +37,18 @@
     resetFlake(n);
     flakes.push(n);
   }
+  
+  (function addFlake() {
+    if (UNITS < 100) {
+      // add an extra flake each 10 seconds
+      setTimeout(addFlake, 10e3);
+    }
+    
+    var n = {};
+    resetFlake(n);
+    flakes.push(n);
+    UNITS++;
+  }());
 
   var raq = window.requestAnimationFrame || window.setTimeout;
 
@@ -49,15 +61,15 @@
     
     // update all flakes
     for (var i = 0; i < UNITS; i++) {
-      var n = flakes[i],
-          xx = n.x + Math.sin(n.z) * 32,
-          nx = xx + width / 2,
-          yy = n.y / n.z,
-          ny = yy,
+      var flake = flakes[i],
+          posX = flake.x + Math.sin(flake.z) * 32,
+          nx = posX + width / 2,
+          posY = flake.y / flake.z,
+          ny = posY,
           // fog alpha fade in
-          alpha = n.z > DEPTH - 5 ? (DEPTH - n.z)/5 : 1,
+          alpha = flake.z > DEPTH - 5 ? (DEPTH - flake.z)/5 : 1,
           // start radius of LOD snowflake
-          rad = DEPTH - n.z - 4;
+          radius = DEPTH - flake.z - 4;
       
       // fade in (fog effect) using alpha value
       ctx.fillStyle = "hsla(0,0%,90%," + alpha + ")";
@@ -68,12 +80,12 @@
       ctx.translate(nx, ny);
     
       // randomize the initial snowflake rotation a bit
-      ctx.rotate(n.s * (i%2 ? 1 : -1) * ((i%3)/2 + 0.1));
+      ctx.rotate(flake.s * (i%2 ? 1 : -1) * ((i%3)/2 + 0.1));
       
       // render basic star style snowflake
-      for (var m=0,g=rad/(i%4+2),h=rad/(i%3+1); m<6; m++) {
+      for (var m=0,g=radius/(i%4+2),h=radius/(i%3+1); m<6; m++) {
         ctx.lineTo(-g, h);
-        ctx.lineTo(0, rad);
+        ctx.lineTo(0, radius);
         ctx.lineTo(g, h);
         ctx.lineTo(0, 0);
         ctx.rotate(Math.PI / 3);
@@ -82,12 +94,12 @@
       
       // render spikes on each spoke
       if (i % 2 == 0) {
-        for (var s = 0, g = rad / (i % 5 + 1); s < 6; s++) {
+        for (var s = 0, g = radius / (i % 5 + 1); s < 6; s++) {
             ctx.beginPath();
-            ctx.moveTo(0, rad);
-            ctx.lineTo(-g, rad-g);
-            ctx.lineTo(0, rad-g);
-            ctx.lineTo(g, rad-g);
+            ctx.moveTo(0, radius);
+            ctx.lineTo(-g, radius-g);
+            ctx.lineTo(0, radius-g);
+            ctx.lineTo(g, radius-g);
             ctx.fill();
             ctx.rotate(Math.PI / 3);
         }
@@ -96,15 +108,15 @@
       ctx.restore();
       
       // update flake position and sinewave offset state
-      n.px = xx;
-      n.py = yy;
-      n.z -= Z;
-      n.s += Math.random() / 5;
+      flake.px = posX;
+      flake.py = posY;
+      flake.z -= DELTA;
+      flake.s += Math.random() / 5;
       
       // when flake is out of the view field
-      if (n.z < Z || n.px < -width || n.px > width || n.py > height) {
+      if (flake.z < DELTA || flake.px < -width || flake.px > width || flake.py > height) {
         // reset flake
-        resetFlake(n);
+        resetFlake(flake);
       }
     }
   }());
